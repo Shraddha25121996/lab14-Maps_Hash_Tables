cc7cc2e25a543412a0fe1c7186ab02cc4ee1d67b
Shraddha25121996 <shraddha25121996@gmail.com>
2025-12-02 15:15:28 -0800

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shraddhapatel
+ */
+class Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() { return key; }
+    public V getValue() { return value; }
+    public V setValue(V value) { 
+        V old = this.value;
+        this.value = value;
+        return old;
+    }
+}
